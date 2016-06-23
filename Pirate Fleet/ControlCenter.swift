23717df83a58786a06b1558b20fdf35c440f7359
uashraf90 <uashraf@outlook.com>
2016-06-23 17:19:31 -0400

    Added my Ships to the game

--- Pirate Fleet/ControlCenter.swift
+++ Pirate Fleet/ControlCenter.swift
@@ -20,6 +20,20 @@ struct Ship {
 class ControlCenter {
     
     func addShipsAndMines(human: Human) {
+        
+        let smallShip1 = Ship(length: 2, location: GridLocation(x:5, y:0) , isVertical: false)
+        let mediumShip1 = Ship(length: 3, location: GridLocation(x:2, y:2) , isVertical: false)
+        let mediumShip2 = Ship(length: 3, location: GridLocation(x:3, y:3), isVertical: false)
+        let largShip1 = Ship(length: 4, location: GridLocation(x:3, y:5), isVertical: false)
+        let extraLargeShip = Ship(length: 5, location: GridLocation(x:3, y:6), isVertical: false)
+        
+        human.addShipToGrid(smallShip1)
+        human.addShipToGrid(mediumShip1)
+        human.addShipToGrid(mediumShip2)
+        human.addShipToGrid(largShip1)
+        human.addShipToGrid(extraLargeShip)
+        
+        
         // Write your code here!
     }
     
